b1b9d7ed5859dd915c3a85dbad836fdd627de799
Ken <thanhcong.bachngoc@gmail.com>
2012-08-11 14:34:39 -0700

    Blank project with icon and launch image.

---
+++ iOS/omgHelp/omgHelp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  omgHelp
+//
+//  Created by Cong Bach on 11/8/12.
+//  Copyright (c) 2012 VSee Lab, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ iOS/omgHelp/omgHelp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  omgHelp
+//
+//  Created by Cong Bach on 11/8/12.
+//  Copyright (c) 2012 VSee Lab, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
